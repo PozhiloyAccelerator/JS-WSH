bc92c51e9f6cf6db7b0dc7a13ce89a389ff8d8a1
PozhiloyAccelerator <dedurinaroslav@gmail.com>
2022-12-19 08:31:41 +0300

    цезарь

--- caesarMolodec/caesar-enc-dec.js
+++ caesarMolodec/caesar-enc-dec.js
@@ -1,8 +1,5 @@
 //%DebugArguments%=/encode /decode
-//cscript caesar-enc-dec.js /encode или /decode "текст" "число сдвига"
-//у нас три основные проюлемы: 1) проверка, что элемент строки из английского алфавита
-//2) считывание строки из файла, надо чтобы он нормально работал со считанной строкой
-//3) он почему - то неправильно шифрует. пример: aaabbb 1 должно быть: bbbccc результат: bbblll
+//cscript caesar-enc-dec.js /encode или /decode "число сдвига"
 
 var args = WScript.Arguments;
 var caesarCode = function(str, offset) {   //offset - сдвиг по алфавиту
@@ -12,7 +9,6 @@ var caesarCode = function(str, offset) {   //offset - сдвиг по алфав
   var result = "";      //ответ
   for (var i = 0; i < str.length; i++) {    //идем по элементам строки
     var encodeSymbol = str.charAt(i); //элемент строки
-    WScript.Echo(encodeSymbol);
     // английский алфавит, тут должна быть проверка на английский алфавит, а в декоде можно убрать
     var code = str.charCodeAt(i);   //получаем код
     if (encodeSymbol.match(/[a-z]/i)) {
@@ -33,6 +29,7 @@ if (args.length === 0) { //если не задано аргументов
 //КОДИРОВКА
 else {
   if (args(0) === "/encode") {
+    var alphabetShift = Number(args(1));
     var p = new ActiveXObject("Scripting.FileSystemObject");
     if (!p.FileExists("input.txt")) { //есть ли файл ввода
       WScript.Echo("no input file!");
@@ -43,7 +40,7 @@ else {
     f.Close();
   }
   var f = p.OpenTextFile("output.txt", 2, true, 0); //выводим ответ
-  f.Write(caesarCode(s, args(1)));
+  f.Write(caesarCode(s, alphabetShift));
   f.Close();
   //ДЕКОДИРОВКА
   if (args(0) === "/decode") {
@@ -65,10 +62,10 @@ else {
       if (decodeSymbol.match(/[a-z]/i)) {
         var code = s.charCodeAt(i);   //получаем код закодированного символа
         if (code >= 65 && code <= 90) {  // заглавные буквы
-          decodeSymbol = String.fromCharCode(((code - 65 - args(2)) % 26) + 65);
+          decodeSymbol = String.fromCharCode(((code - 65 - alphabetShift) % 26) + 65);
         }
         else if (code >= 97 && code <= 122) { // строчные буквы
-          decodeSymbol = String.fromCharCode(((code - 97 - args(2)) % 26) + 97);
+          decodeSymbol = String.fromCharCode(((code - 97 - alphabetShift) % 26) + 97);
         }
       }
       result += decodeSymbol;   // записываем расшифрованный символ в ответ
@@ -77,61 +74,4 @@ else {
     f.Write(result);
     f.Close();
   }
-  else {
-    WScript.Echo("wrong argument!");
-  }
 }
-
-
-/*
-var args = WScript.Arguments;
-var caesarCode = function(str, offset) { //offset - сдвиг по алфавиту
-  if (offset < 0) { // если сдвиг меньше 0
-    return caesarCode(str, offset + 26);
-  }
-  var result = ""; //ответ
-  for (var i = 0; i < str.length; i++) { //идем по элементам строки
-    var encodeSymbol = str[i] ; //элемент строки
-    // английский алфавит
-    var code = str.charCodeAt(i); //получаем код
-    if (code >= 65 && code <= 90) { // заглавные буквы
-      encodeSymbol = String.fromCharCode(((code - 65 + offset) % 26) + 65);
-    }
-    else if (code >= 97 && code <= 122) { // строчные буквы
-      encodeSymbol = String.fromCharCode(((code - 97 + offset) % 26) + 97);
-    }
-    result += encodeSymbol; // записываем зашифрованный символ в ответ
-  }
-  return result;
-}
-
-if (args.length === 0) { //если не задано аргументов
-  WScript.Echo("wrong arguments!");
-}
-//КОДИРОВКА
-else {
-  if (args(0) === "/encode") {
-    var encodeLine = caesarCode(args(1), args(2));
-    WScript.Echo(encodeLine);
-  }
-  //ДЕКОДИРОВКА
-  else if (args(0) === "/decode") {
-
-    var result = ""; //ответ
-    for (var i = 0; i < encodeLine.length; i++) { //идем по элементам закодированной строки
-      var decodeSymbol = encodeLine[i]; //элемент закодированной строки
-      // английский алфавит и цифры
-      if (decodeSymbol.match(/[a-z]/i)) {
-        var code = encodeLine.charCodeAt(i); //получаем код закодированного символа
-        if (code >= 65 && code <= 90) { // заглавные буквы
-          decodeSymbol = String.fromCharCode(((code - 65 - args(2)) % 26) + 65);
-        }
-        else if (code >= 97 && code <= 122) { // строчные буквы
-          decodeSymbol = String.fromCharCode(((code - 97 - args(2)) % 26) + 97);
-        }
-      }
-      result += decodeSymbol; // записываем разшифрованный символ в ответ
-    }
-    WScript.Echo(result);
-  }
-}      */
